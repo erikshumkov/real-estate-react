02b9617f44365081f82082889ce4fad573b6e4e0
Erik Shumkov <erikshumkov@gmail.com>
2020-01-19 17:37:08 +0100

    Updates

---
+++ precache-manifest.56593ffc221beafba5914e79f25e6103.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f743b202bbf7e0f409dd46236b0a40",
+    "url": "/real-estate-react/index.html"
+  },
+  {
+    "revision": "f725ec77118926c8fdb3",
+    "url": "/real-estate-react/static/css/main.446142ef.chunk.css"
+  },
+  {
+    "revision": "02d31eed23096d8c4421",
+    "url": "/real-estate-react/static/js/2.bb391ac7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/real-estate-react/static/js/2.bb391ac7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f725ec77118926c8fdb3",
+    "url": "/real-estate-react/static/js/main.4390ac4b.chunk.js"
+  },
+  {
+    "revision": "6a785479c5d867871220",
+    "url": "/real-estate-react/static/js/runtime-main.b5ac8807.js"
+  },
+  {
+    "revision": "b297e4f5e4f0b04fab476c13406f2208",
+    "url": "/real-estate-react/static/media/apartment.b297e4f5.jpg"
+  },
+  {
+    "revision": "7d7f92f9a2f9fdfb7adeb9b96e47d59b",
+    "url": "/real-estate-react/static/media/apartment10.7d7f92f9.jpg"
+  },
+  {
+    "revision": "26a8a009ae71f81594153a09fd144305",
+    "url": "/real-estate-react/static/media/apartment11.26a8a009.jpg"
+  },
+  {
+    "revision": "1aa170cdd88d5542c342e2a73246ed58",
+    "url": "/real-estate-react/static/media/apartment12.1aa170cd.jpg"
+  },
+  {
+    "revision": "82d78c5a5b08b921165679a690500e48",
+    "url": "/real-estate-react/static/media/apartment13.82d78c5a.jpg"
+  },
+  {
+    "revision": "6eab7598a5ec9dbea5a11fe3d9ef8a09",
+    "url": "/real-estate-react/static/media/apartment2.6eab7598.jpg"
+  },
+  {
+    "revision": "e88a8628541fd272e45ae25d613b14c7",
+    "url": "/real-estate-react/static/media/apartment3.e88a8628.jpg"
+  },
+  {
+    "revision": "4374e65def9c7f77d60bb784ce08a8bb",
+    "url": "/real-estate-react/static/media/apartment4.4374e65d.jpg"
+  },
+  {
+    "revision": "0af07f82d98678dc20c36f6b210938ec",
+    "url": "/real-estate-react/static/media/apartment5.0af07f82.jpg"
+  },
+  {
+    "revision": "ca74f72a7a4f4ef3c55753feb4b92e91",
+    "url": "/real-estate-react/static/media/apartment6.ca74f72a.jpg"
+  },
+  {
+    "revision": "57676eab12507418b249916717a669a2",
+    "url": "/real-estate-react/static/media/apartment7.57676eab.jpg"
+  },
+  {
+    "revision": "e85f71b613ba1d2541b094b201f09433",
+    "url": "/real-estate-react/static/media/apartment8.e85f71b6.jpg"
+  },
+  {
+    "revision": "f7c06381b6863374df9ae57fcf648ab8",
+    "url": "/real-estate-react/static/media/apartment9.f7c06381.jpg"
+  }
+]);
