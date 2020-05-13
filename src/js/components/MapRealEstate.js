4d8c28c09f7c91c0daf896fbab82161b71781441
Erik Shumkov <erikshumkov@gmail.com>
2020-05-13 13:26:03 +0200

    add functionality to list items in map page

--- src/js/components/MapRealEstate.js
+++ src/js/components/MapRealEstate.js
@@ -2,7 +2,7 @@ import React from 'react'
 import L from "leaflet";
 import { Map, Marker, TileLayer } from "react-leaflet";
 
-const MapRealEstate = ({ data, location, getItemOnClick }) => {
+const MapRealEstate = ({ data, location, getItemOnClick, refs2 }) => {
   // Get pathname
   let path = location.pathname;
   // Check if the route / path is true
@@ -23,26 +23,87 @@ const MapRealEstate = ({ data, location, getItemOnClick }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
 
-      {ifPath ? (data.map(home => (
-        <Marker
-          key={home.id}
-          icon={icon}
-          onmouseover={(e) => {
-            e.target._icon.firstChild.style.backgroundColor = "#0e6a88";
-            e.target._icon.firstChild.style.transform = "scale(1.2) rotate(-45deg)";
-          }}
-          onmouseout={(e) => {
-            e.target._icon.firstChild.style.backgroundColor = "#1493bc";
-            e.target._icon.firstChild.style.transform = "scale(1) rotate(-45deg)";
-          }}
-          position={
-            [
-              home.coords.lat,
-              home.coords.lng
-            ]}
-          onclick={() => getItemOnClick(home.id)}
-        />
-      ))) : (
+      {ifPath ? (data.map(home => {
+        if (home.homeType === "villa" || home.homeType === "radhus") {
+          return <Marker
+            key={home.id}
+            ref={refs2[home.id]}
+            icon={L.divIcon({
+              className: "custom-div-icon",
+              html: "<div style='background-color:#f9c006;' class='marker-pin'></div><i class='material-icons'>home</i>",
+              iconSize: [30, 42],
+              iconAnchor: [15, 42]
+            })}
+            onmouseover={(e) => {
+              // e.target._icon.firstChild.style.backgroundColor = "#0e6a88";
+              e.target._icon.firstChild.style.transform = "scale(1.2) rotate(-45deg)";
+            }}
+            onmouseout={(e) => {
+              // e.target._icon.firstChild.style.backgroundColor = "#1493bc";
+              e.target._icon.firstChild.style.transform = "scale(1) rotate(-45deg)";
+            }}
+            position={
+              [
+                home.coords.lat,
+                home.coords.lng
+              ]}
+            onclick={() => getItemOnClick(home.id)}
+          />
+        }
+        if (home.homeType === "apartment") {
+          return <Marker
+            key={home.id}
+            ref={refs2[home.id]}
+            icon={L.divIcon({
+              className: "custom-div-icon",
+              html: "<div style='background-color:#fb3640;' class='marker-pin'></div><i class='material-icons'>apartment</i>",
+              iconSize: [30, 42],
+              iconAnchor: [15, 42]
+            })}
+            onmouseover={(e) => {
+              // e.target._icon.firstChild.style.backgroundColor = "#0e6a88";
+              e.target._icon.firstChild.style.transform = "scale(1.2) rotate(-45deg)";
+            }}
+            onmouseout={(e) => {
+              // e.target._icon.firstChild.style.backgroundColor = "#1493bc";
+              e.target._icon.firstChild.style.transform = "scale(1) rotate(-45deg)";
+            }}
+            position={
+              [
+                home.coords.lat,
+                home.coords.lng
+              ]}
+            onclick={() => getItemOnClick(home.id)}
+          />
+        }
+        if (home.homeType === "fritidshus") {
+          return <Marker
+            key={home.id}
+            ref={refs2[home.id]}
+            icon={L.divIcon({
+              className: "custom-div-icon",
+              html: "<div style='background-color:#257bc1;' class='marker-pin'></div><i class='material-icons'>home</i>",
+              iconSize: [30, 42],
+              iconAnchor: [15, 42]
+            })}
+            onmouseover={(e) => {
+              e.target._icon.firstChild.style.backgroundColor = "#257bc1";
+              e.target._icon.firstChild.style.transform = "scale(1.2) rotate(-45deg)";
+            }}
+            onmouseout={(e) => {
+              // e.target._icon.firstChild.style.backgroundColor = "#1493bc";
+              e.target._icon.firstChild.style.transform = "scale(1) rotate(-45deg)";
+            }}
+            position={
+              [
+                home.coords.lat,
+                home.coords.lng
+              ]}
+            onclick={() => getItemOnClick(home.id)}
+          />
+        }
+        return null;
+      })) : (
           <Marker
             icon={icon}
             position={[data.coords.lat, data.coords.lng]}
--- src/js/pages/MapPage.js
+++ src/js/pages/MapPage.js
@@ -10,6 +10,11 @@ const MapPage = ({ data, location }) => {
     return acc;
   }, {});
 
+  const refs2 = data.reduce((acc, value) => {
+    acc[value.id] = React.createRef();
+    return acc;
+  }, {});
+
   const getItemOnClick = id => {
     // Remove border from each item
     for (const ref in refs) {
@@ -26,9 +31,23 @@ const MapPage = ({ data, location }) => {
     li.style.border = "2px solid black";
   }
 
-  // const func = (e, id) => {
+  // Save initial z index of map markers
+  let initZindex;
 
-  // }
+  // Add style to map markers on hover list item.
+  const enterLi = (id) => {
+    let style = refs2[id].current.leafletElement._icon.firstChild.style;
+    initZindex = refs2[id].current.leafletElement._icon.style.zIndex;
+    refs2[id].current.leafletElement._icon.style.zIndex = 1000;
+    style.transform = "scale(1.2) rotate(-45deg)";
+  }
+
+  // Remove styles when you leave list item.
+  const leaveLi = (id) => {
+    let style = refs2[id].current.leafletElement._icon.firstChild.style;
+    refs2[id].current.leafletElement._icon.style.zIndex = initZindex;
+    style.transform = "scale(1) rotate(-45deg)";
+  }
 
 
   return (
@@ -38,6 +57,8 @@ const MapPage = ({ data, location }) => {
 
           {data.map(home => (
             <Link
+              onMouseEnter={() => enterLi(home.id)}
+              onMouseLeave={() => leaveLi(home.id)}
               key={home.id}
               ref={refs[home.id]}
               className="home-link"
@@ -64,7 +85,7 @@ const MapPage = ({ data, location }) => {
 
         </div>
         <div className="map-container">
-          <MapRealEstate data={data} location={location} getItemOnClick={getItemOnClick} />
+          <MapRealEstate data={data} location={location} getItemOnClick={getItemOnClick} refs2={refs2} />
         </div>
       </div>
     </div>
