cdf4ed84016011c23e2b1336ed4b0a951c5366e4
Erik Shumkov <erikshumkov@gmail.com>
2020-05-18 20:58:50 +0200

    change footer

--- src/js/layout/Footer.js
+++ src/js/layout/Footer.js
@@ -7,12 +7,30 @@ const Footer = () => {
         <div className='wrapper'>
           <div className='footer-grid'>
             <div className='footer-contact'>
+              <h2>Sälj med oss</h2>
+              <ul>
+                <li>Fri värdering</li>
+                <li>Säkert köp</li>
+                <li>Besiktning</li>
+                <li>Fotografering</li>
+              </ul>
+            </div>
+            <div className='footer-contact'>
+              <h2>Köpa Bostad</h2>
+              <ul>
+                <li>Till salu</li>
+                <li>Bevaka objekt</li>
+                <li>Nyproduktion</li>
+                <li>Komersiella</li>
+              </ul>
+            </div>
+            <div className='footer-contact'>
               <h2>Kontakt</h2>
               <ul>
-                <li>Kontakta oss</li>
-                <li>Om oss</li>
+                <li>Hitta mäklare</li>
                 <li>Jobba hos oss</li>
-                <li>Om bostadsmarknaden</li>
+                <li>Kunders omdömen</li>
+                <li>Press</li>
               </ul>
             </div>
             <div className='footer-social'>
@@ -35,16 +53,6 @@ const Footer = () => {
                 </li>
                 <li></li>
               </ul>
-            </div>
-            <div className='footer-about'>
-              <h2>Om oss</h2>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa,
-                sed, totam quam, magni velit a culpa eligendi vel iure nam quod
-                ad quae error beatae? Animi ex aperiam quasi nobis? Lorem ipsum,
-                dolor sit amet consectetur adipisicing elit. Quos tempore beatae
-                dolor voluptatem.
-              </p>
             </div>
           </div>
         </div>
