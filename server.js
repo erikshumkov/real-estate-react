8d3d0211d6e78ff36da9c6c6912c6d7e154ad6e2
Erik Shumkov <erikshumkov@gmail.com>
2021-02-09 12:03:48 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/profile", require("./routes/profile"))
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"))
+  app.use(express.static(path.join(__dirname, "/client/build")))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
