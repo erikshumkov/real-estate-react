4c6fb9fc8abb05709b3915c7530dbe666881aac1
Erik Shumkov <erikshumkov@gmail.com>
2021-02-09 11:03:46 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const connectMDB = require("./config/db")
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
@@ -18,6 +19,16 @@ app.use("/api/posts", require("./routes/posts"))
 app.use("/api/cities", require("./routes/cities"))
 app.use("/api/users", require("./routes/users"))
 app.use("/api/profile", require("./routes/profile"))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
