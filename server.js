e7f15088b2d91c969855e7e8dc2d1aa902231613
Erik Shumkov <erikshumkov@gmail.com>
2021-02-10 10:49:16 +0100

    prepare deployment

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"))
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  // })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
 }
 
 const PORT = process.env.PORT || 5000
