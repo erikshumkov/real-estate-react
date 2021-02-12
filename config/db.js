86cc9b79b90ba359afa12d646dbfc236b98df791
Erik Shumkov <erikshumkov@gmail.com>
2021-02-12 13:11:06 +0100

    deployment

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const config = require("config")
 
 const connectMDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
